ed7c1213dba361e1aa5dfd6422965ac81acf1b7a
Jaedan Persaud <63131031+jaedanpersaud12@users.noreply.github.com>
2022-03-15 15:10:26 -0400

    Updated tailwind to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,39 +2,39 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	mode: 'jit',
-	content: ["./build/**/*.html"],
-	theme: {
-		fontFamily: {
-			inter: ["Inter", ...defaultTheme.fontFamily.sans],
-			pj: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
-			vol: ["volund"],
-			rec: ["recoleta"]
-		},
-		
+    mode: 'jit',
+    content: ["./build/**/*.html"],
+    theme: {
+        fontFamily: {
+            inter: ["Inter", ...defaultTheme.fontFamily.sans],
+            pj: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+            vol: ["volund"],
+            rec: ["recoleta"]
+        },
 
-		extend: {
-			colors: {
-				amber: colors.amber,
-				teal: colors.teal,
-				orange: colors.orange,
-				rose: colors.rose,
-				"wf-pink": '#F16278',
-				"wf-peach": '#EFB8A8',
-				"wf-black": '#341018'
-			},
-		},
-	},
-	variants: {
-		extend: {
-			fontFamily: {
-			
-			}
-		},
-	},
-	plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio")],
-	corePlugins: {
-		animation: false,
-		float: false,
-	},
+
+        extend: {
+            colors: {
+                amber: colors.amber,
+                teal: colors.teal,
+                orange: colors.orange,
+                rose: colors.rose,
+                "wf-pink": '#F16278',
+                "wf-peach": '#EFB8A8',
+                "wf-black": '#341018'
+            },
+        },
+    },
+    variants: {
+        extend: {
+            fontFamily: {
+
+            }
+        },
+    },
+    plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio")],
+    corePlugins: {
+        animation: false,
+        float: false,
+    },
 };
